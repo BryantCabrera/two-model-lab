a55ff5905e60789215aa45eae357850178967103
Bryant <cabrera.bryant@gmail.com>
2019-01-22 14:38:20 -0800

    Creates Home Page

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 require('./db/db.js');
+const Cities = require('./models/cities');
+
+app.get('/', (req, res) => {
+    Cities.find({}, (err, allCities) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(allCities);
+            res.render('../views/cities/index.ejs');
+        }
+    });
+});
+
 
 app.listen(8000, () => {
     console.log('Server listening on port 8000.');
